6a5b0c3ba3021c2903526dbfe8b459105a5ebef3
Nick Hill <nickhill2380@gmail.com>
2021-03-13 16:17:00 -0500

    tested modal

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -1,8 +1,7 @@
-// __tests__/Contact.test.js
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import Contact from './Contact'
+import Contact from '..'
 
 afterEach(cleanup)
 
